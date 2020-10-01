99a0a84873cb92311fdc1cd762b2a3bfd6369509
Kermalis <29823718+Kermalis@users.noreply.github.com>
2020-10-01 12:48:34 -0400

    Expose InstrumentHeaderChunk

--- DLS2/Chunks/InstrumentHeaderChunk.cs
+++ DLS2/Chunks/InstrumentHeaderChunk.cs
@@ -1,18 +1,32 @@
 ﻿using Kermalis.EndianBinaryIO;
+using System;
 
 namespace Kermalis.DLS2
 {
     // Instrument Header Chunk - Page 45 of spec
     public sealed class InstrumentHeaderChunk : DLSChunk
     {
-        public uint NumRegions { get; }
-        public MIDILocale Locale { get; }
+        public uint NumRegions { get; set; }
+        private MIDILocale _locale;
+        public MIDILocale Locale
+        {
+            get => _locale;
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                _locale = value;
+            }
+        }
 
+        public InstrumentHeaderChunk() : base("insh") { }
         internal InstrumentHeaderChunk(EndianBinaryReader reader) : base("insh", reader)
         {
             long endOffset = GetEndOffset(reader);
             NumRegions = reader.ReadUInt32();
-            Locale = new MIDILocale(reader);
+            _locale = new MIDILocale(reader);
             EatRemainingBytes(reader, endOffset);
         }
 
@@ -26,7 +40,7 @@ namespace Kermalis.DLS2
         {
             base.Write(writer);
             writer.Write(NumRegions);
-            Locale.Write(writer);
+            _locale.Write(writer);
         }
     }
 }
--- DLS2/Structs/MIDILocale.cs
+++ DLS2/Structs/MIDILocale.cs
@@ -1,23 +1,85 @@
 ﻿using Kermalis.EndianBinaryIO;
+using System;
 
 namespace Kermalis.DLS2
 {
     // MIDILOCALE - Page 45 of spec
     public sealed class MIDILocale
     {
-        public uint Bank { get; set; } // TODO: bits
-        public uint Instrument { get; set; }
+        public uint Bank_Raw { get; set; }
+        public uint Instrument_Raw { get; set; }
 
+        public byte CC32
+        {
+            get => (byte)(Bank_Raw & 127);
+            set
+            {
+                if (value > 127)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                Instrument_Raw |= value;
+            }
+        }
+        public byte CC0
+        {
+            get => (byte)((Bank_Raw >> 7) & 127);
+            set
+            {
+                if (value > 127)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                Instrument_Raw |= (uint)(value << 7);
+            }
+        }
+        public bool IsDrum
+        {
+            get => (Bank_Raw >> 31) != 0;
+            set
+            {
+                const uint val = 1u << 31;
+                if (value)
+                {
+                    Instrument_Raw |= val;
+                }
+                else
+                {
+                    Instrument_Raw &= ~val;
+                }
+            }
+        }
+        public byte Instrument
+        {
+            get => (byte)(Instrument_Raw & 127);
+            set
+            {
+                if (value > 127)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                Instrument_Raw |= value;
+            }
+        }
+
+        public MIDILocale() { }
+        public MIDILocale(byte cc32, byte cc0, bool isDrum, byte instrument)
+        {
+            CC32 = cc32;
+            CC0 = cc0;
+            IsDrum = isDrum;
+            Instrument = instrument;
+        }
         internal MIDILocale(EndianBinaryReader reader)
         {
-            Bank = reader.ReadUInt32();
-            Instrument = reader.ReadUInt32();
+            Bank_Raw = reader.ReadUInt32();
+            Instrument_Raw = reader.ReadUInt32();
         }
 
         internal void Write(EndianBinaryWriter writer)
         {
-            writer.Write(Bank);
-            writer.Write(Instrument);
+            writer.Write(Bank_Raw);
+            writer.Write(Instrument_Raw);
         }
     }
 }
